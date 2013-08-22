b9b4db0aebfe897c375b928d170c8646c96f4ca4
Dan Tillberg <dan@tillberg.us>
2013-08-22 05:19:45 +0000

    The demo is way too long, but it's in a repeatable pattern now.

--- _includes/js/index.js
+++ _includes/js/index.js
@@ -1,61 +1,31 @@
 ;
 
-(function () {
-    return;
-    tbone.createModel('stapler').singleton();
-    tbone('stapler.brand', 'Swingline');
-    tbone.createView('input', function () {
-        var $this = this.$el;
-        var prop = this.$el.data().value;
-        var interacting;
-        tbone(function () {
-            var value = tbone(prop);
-            if (!interacting) {
-                $this.val(value || '');
-            }
-            interacting = false;
-        });
-        function update() {
-            interacting = true;
-            tbone(prop, $this.val());
-        }
-        $this.keydown(update).keyup(update).change(update).blur(update);
-    });
-    tbone.createView('onMyDesk', function() {
-        this.$('stapler').css('background', tbone('stapler.color'));
-    });
-    var timer;
-    $('.example-1 button').click(function () {
-        if (timer) { clearTimeout(timer); timer = null; }
-        var choices = [
-            'red', 'hotpink', '#f00', '#bada55', 'rgba(255, 0, 0, 1)',
-            'aquamarine', 'hsl(0, 100%, 50%)'
-        ];
-        var choice = choices[Math.floor(choices.length * Math.random())];
-        var typed = '';
-        function another () {
-            typed += choice.charAt(0);
-            T('stapler.color', typed);
-            choice = choice.substr(1);
-            timer = choice ? setTimeout(another, 40 + 100 * Math.random()) : null;
-        }
-        another();
-    });
-    tbone.render($('[tbone]'));
-    $('body').addClass('tbone-ready');
-}());
+window.demoCallbacks = {};
 
 tbone.createView('example', function () {
-    this.$('fragment').each(function () {
+    var self = this;
+    function updateFragmentHeaderStyle () {
         var $this = $(this);
-        var height = $this.children('pre').height();
+        var height = $this.children('section').height();
         var margin = (height / 2) - 13;// 13 is the width / 2
         $this.children('h3').css({
             width: height,
             marginLeft: -margin,
             marginTop: margin
         });
-    });
+    }
+    self.$('fragment').each(updateFragmentHeaderStyle);
+    window.demoCallbacks[self.query('id')] = function (op, data) {
+        if (op === 'init') {
+            return {
+                html: self.query('html'),
+                javascript: self.query('javascript')
+            };
+        } else if (op === 'setcss') {
+            self.$('iframe').css(data);
+            updateFragmentHeaderStyle.call(self.$('iframe').closest('fragment')[0]);
+        }
+    };
 });
 
 (function () {
@@ -75,16 +45,18 @@ tbone.createView('example', function () {
         });
     }
 
-    $('script[type="text/example"]').each(function () {
+    $('script[type="text/example"]').each(function (i) {
         var $this = $(this);
         var src = _.trim($this.html());
         var currType;
-        var parts = {};
-        src.replace(/\[(\w+)\]|([^\[]+)/g, function (all, newType, newContents) {
+        var parts = {
+            id: i
+        };
+        src.replace(/~(\w+)~|([^~]+)/g, function (all, newType, newContents) {
             if (newType) {
                 currType = newType;
             } else {
-                parts[currType] = _.trim(newContents);
+                parts[currType] = _.trim(newContents).replace(/scrpt/g, 'script');
                 var highlighted = hljs.highlightAuto(parts[currType]).value;
                 if (currType === 'html') {
                     highlighted = highlightInlineJS(highlighted);
