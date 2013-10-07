f8122a93d2c02a7ef56fbce37f6b1d2f33df3011
Dan Tillberg <dan@tillberg.us>
2013-10-07 02:04:45 +0000

    Add beginning of presentation

---
+++ _includes/js/preso.js
@@ -0,0 +1,65 @@
+;
+
+(function () {
+    $('script[type="text/slide"]').each(function (i) {
+        var $this = $(this);
+        var src = _.trim($this.html());
+        var currType;
+        var name = 'slide_' + i;
+        var parts = {
+            id: i,
+            name: name
+        };
+        src.replace(/~(\w+)~|([^~]+)/g, function (all, newType, newContents) {
+            if (newType) {
+                currType = newType;
+            } else {
+                parts[currType] = _.trim(newContents).replace(/scrpt/g, 'script');
+            }
+        });
+        if (parts.javascript) {
+            tbone.createView(name, new Function(parts.javascript));
+        }
+        tbone.addTemplate(name, parts.html);
+        T.push('slides', parts);
+    });
+}());
+
+T('slideNumber', function () {
+    return parseFloat((T('location.hash') || '#0').replace(/^#/, ''));
+});
+
+tbone.createView('preso', function () {
+    var self = this;
+    var $slides = self.$('slide');
+    T(function () {
+        var width = T('screen.width');
+        var height = T('screen.height');
+        $slides.css({
+            width: width,
+            height: height
+        });
+        T(function () {
+            var slideNumber = T('slideNumber') || 0;
+            $slides.each(function (i) {
+                $(this).css('left', (i - slideNumber) * width);
+            });
+        });
+    });
+});
+
+(function () {
+    function update () {
+        T('screen.width', $(window).width());
+        T('screen.height', $(window).height());
+    }
+    $(window).bind('resize', update);
+    function timer () {
+        update();
+        setTimeout(timer, 1000);
+    }
+    timer();
+}());
+
+T('location', tbone.models.location.make());
+tbone.render($('[tbone]'));
