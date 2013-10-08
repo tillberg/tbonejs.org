67e8a90c8a7d4716e6d43f96839a26940010a465
Dan Tillberg <dan@tillberg.us>
2013-10-08 18:04:05 +0000

    Split build script into website and CDN modules

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 : ${TBONE_S3?"Set TBONE_S3 to TBone's S3 bucket"}
-: ${TBONECDN_S3?"Set TBONECDN_S3 to TBone's CDN S3 bucket"}
 
 set -e
 rm -rf _sitegen/
 rm -rf _cdn/
-mkdir -p _sitegen/
-mkdir -p _cdn/
 
 echo Fetching TBone...
 
@@ -18,25 +15,6 @@ cd _tbone
 git fetch origin
 git checkout master
 git reset --hard origin/master
-for ref in `git ls-remote origin | grep ".*refs/[^p]" | sed "s/.*\///"`; do
-    echo "Building tbone $ref"
-    git checkout -q $ref
-    OPTIMIZATION_LEVEL=ADVANCED_OPTIMIZATIONS ./compile.py > ../_cdn/tbone-$ref.min.js
-    # compile with debug=true has the side effect of generating build/tbone.debug.js
-    TBONE_DEBUG=TRUE OPTIMIZATION_LEVEL=WHITESPACE_ONLY ./compile.py > /dev/null
-    cp build/tbone.debug.js ../_cdn/tbone-$ref.js
-    cp build/tbone.min.js.map ../_cdn/tbone-$ref.min.js.map
-    sed -i s/tbone.min.js.map/tbone-$ref.min.js.map/ ../_cdn/tbone-$ref.min.js
-    if [ -f "package.json" ]; then
-        mkdir -p tbone/
-        cp build/tbone.debug.js tbone/tbone.js
-        cp package.json LICENSE README.md tbone/
-        sed -i "s/build\/tbone\.debug\.js/tbone\.js/" tbone/package.json
-        tar czf ../_cdn/tbone-$ref.tgz tbone/
-        rm -rf tbone/
-    fi
-done
-git checkout -q master
 
 ../node_modules/docco-husky/bin/generate src/
 rm -rf ../docs/
@@ -61,16 +39,6 @@ find _sitegen/ -iname '*.js' -exec gzip -n {} +
 find _sitegen/ -iname '*.css' -exec gzip -n {} +
 find _sitegen/ -iname '*.gz' -exec rename 's/\.gz$//i' {} +
 
-cd _cdn
-for name in `ls *.js | sed s/\.js//`; do
-    # for the non-gzip-encoded version to work right,
-    # we shouldn't set Content-encoding: gzip on them.
-    # cp $name.js $name.raw.js
-    gzip $name.js
-    mv $name.js.gz $name.js
-done
-cd ..
-
 cd _sitegen/
 # Make no-extension copies of all HTML files
 mkdir tmp
@@ -80,9 +48,7 @@ mv tmp/* ./
 rmdir tmp/
 cd ..
 
-echo Syncing to S3...
+echo "Syncing to s3://$TBONE_S3/..."
 
 s3cmd sync --progress --guess-mime-type --no-preserve --acl-public --add-header 'Cache-Control: max-age=60' --add-header 'Content-Encoding:gzip' _sitegen/ s3://$TBONE_S3/ --exclude '*.*' --include '*.html' --include '*.js' --include '*.css'
 s3cmd sync --progress --guess-mime-type --no-preserve --acl-public --add-header 'Cache-Control: max-age=60' _sitegen/ s3://$TBONE_S3/ --exclude '*.html' --exclude '*.js' --exclude '*.css'
-s3cmd sync --progress --guess-mime-type --no-preserve --acl-public --add-header 'Cache-Control: max-age=600' --add-header 'Content-Encoding:gzip' _cdn/ s3://$TBONECDN_S3/ --exclude '*.*' --include 'tbone-master.*'
-s3cmd sync --progress --guess-mime-type --no-preserve --acl-public --add-header 'Cache-Control: max-age=600' --add-header 'Content-Encoding:gzip' _cdn/ s3://$TBONECDN_S3/ --exclude 'tbone-master.*'
---
+++ buildcdn.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+: ${TBONECDN_S3?"Set TBONECDN_S3 to TBone's CDN S3 bucket"}
+
+set -e
+rm -rf _cdn/
+mkdir -p _cdn/
+
+echo Fetching TBone...
+
+if [ ! -d "_tbone" ]; then
+    git clone git@github.com:appneta/tbone.git _tbone
+fi
+cd _tbone
+git fetch origin
+git checkout master
+git reset --hard origin/master
+for ref in `git ls-remote origin | grep ".*refs/[^p]" | sed "s/.*\///"`; do
+    echo "Building tbone $ref"
+    git checkout -q $ref
+    OPTIMIZATION_LEVEL=ADVANCED_OPTIMIZATIONS ./compile.py > ../_cdn/tbone-$ref.min.js
+    # compile with debug=true has the side effect of generating build/tbone.debug.js
+    TBONE_DEBUG=TRUE OPTIMIZATION_LEVEL=WHITESPACE_ONLY ./compile.py > /dev/null
+    cp build/tbone.debug.js ../_cdn/tbone-$ref.js
+    cp build/tbone.min.js.map ../_cdn/tbone-$ref.min.js.map
+    sed -i s/tbone.min.js.map/tbone-$ref.min.js.map/ ../_cdn/tbone-$ref.min.js
+    if [ -f "package.json" ]; then
+        mkdir -p tbone/
+        cp build/tbone.debug.js tbone/tbone.js
+        cp package.json LICENSE README.md tbone/
+        sed -i "s/build\/tbone\.debug\.js/tbone\.js/" tbone/package.json
+        tar czf ../_cdn/tbone-$ref.tgz tbone/
+        rm -rf tbone/
+    fi
+done
+git checkout -q master
+cd ..
+
+cd _cdn
+for name in `ls *.js | sed s/\.js//`; do
+    # for the non-gzip-encoded version to work right,
+    # we shouldn't set Content-encoding: gzip on them.
+    # cp $name.js $name.raw.js
+    gzip $name.js
+    mv $name.js.gz $name.js
+done
+cd ..
+
+echo "Syncing to s3://$TBONECDN_S3/..."
+
+s3cmd sync --progress --guess-mime-type --no-preserve --acl-public --add-header 'Cache-Control: max-age=600' --add-header 'Content-Encoding:gzip' _cdn/ s3://$TBONECDN_S3/ --exclude '*.*' --include 'tbone-master.*'
+s3cmd sync --progress --guess-mime-type --no-preserve --acl-public --add-header 'Cache-Control: max-age=600' --add-header 'Content-Encoding:gzip' _cdn/ s3://$TBONECDN_S3/ --exclude 'tbone-master.*'
