879ed9bccf91ebf1bf773e39daee40632757b7b9
Dan Tillberg <dan.tillberg@threatstack.com>
2015-02-07 18:09:31 -0500

    Update cdn build/deploy script for tbone 1.0.0 (and for OSX compatibility)

--- buildcdn.sh
+++ buildcdn.sh
@@ -19,24 +19,39 @@ for ref in `git ls-remote origin | grep ".*refs/[^p]" | sed "s/.*\///" | sort -r
     echo "Building tbone $ref"
     git checkout -q $ref
     if [ -f "compile.py" ]; then
+        # echo "Using compile.py build method..."
         # Old build method, 0.5 and older
         OPTIMIZATION_LEVEL=ADVANCED_OPTIMIZATIONS ./compile.py > ../_cdn/tbone-$ref.min.js
         # compile with debug=true has the side effect of generating build/tbone.debug.js
         TBONE_DEBUG=TRUE OPTIMIZATION_LEVEL=WHITESPACE_ONLY ./compile.py > /dev/null
         cp build/tbone.debug.js ../_cdn/tbone-$ref.js
         cp build/tbone.min.js.map ../_cdn/tbone-$ref.min.js.map
+    elif [ -f "gulpfile.js" ]; then
+        # echo "Using gulp build method..."
+        npm install
+        gulp
+        cp dist/tbone.js ../_cdn/tbone-$ref.js
+        cp dist/tbone.min.js ../_cdn/tbone-$ref.min.js
+        cp dist/tbone.min.js.map ../_cdn/tbone-$ref.min.js.map
+        cp dist/tbone_legacy.js ../_cdn/tbone_legacy-$ref.js
+        cp dist/tbone_legacy.min.js ../_cdn/tbone_legacy-$ref.min.js
+        cp dist/tbone_legacy.min.js.map ../_cdn/tbone_legacy-$ref.min.js.map
     else
+        # echo "Using dist/ build..."
         cp dist/tbone.js ../_cdn/tbone-$ref.js
         cp dist/tbone.min.js ../_cdn/tbone-$ref.min.js
         cp dist/tbone.min.js.map ../_cdn/tbone-$ref.min.js.map
     fi
-    sed -i s/tbone.min.js.map/tbone-$ref.min.js.map/ ../_cdn/tbone-$ref.min.js
+    sed "s/tbone.min.js.map/tbone-$ref.min.js.map/" "../_cdn/tbone-$ref.min.js" > .tmp
+    mv .tmp "../_cdn/tbone-$ref.min.js"
     if [ -f "package.json" ]; then
         mkdir -p tbone/
         cp ../_cdn/tbone-$ref.js tbone/tbone.js
         cp package.json LICENSE README.md tbone/
-        sed -i "s/build\/tbone\.debug\.js/tbone\.js/" tbone/package.json
-        sed -i "s/dist\/tbone\.js/tbone\.js/" tbone/package.json
+        sed "s/build\/tbone\.debug\.js/tbone\.js/" tbone/package.json > .tmp
+        mv .tmp tbone/package.json
+        sed "s/dist\/tbone\.js/tbone\.js/" tbone/package.json > .tmp
+        mv .tmp tbone/package.json
         tar czf ../_cdn/tbone-$ref.tgz tbone/
         rm -rf tbone/
     fi
